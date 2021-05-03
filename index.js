157e7e7d6b5dc39bc8143e56b6b561c1abb97b8e
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-03 18:27:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ function add (a,b) { return a + b; }
 function subtract (a,b) { return a - b; }
 function multiply (a,b) { return a * b; }
 function divide (a,b) { return a / b; }
-function increment(n) { return n++; }
-function decrement(n) { return n--; }
+
+function increment(n) { 
+  n++;
+  return n; }
+function decrement(n) { 
+  n--; 
+  return n; }
 
 console.log(decrement(5))
